7ad356d130073351715f173487917b67247cd541
Kaiwinta <131403480+Kaiwinta@users.noreply.github.com>
2026-04-06 15:24:30 +0200

    feat: ading word db and script for teh word of the day

---
+++ Readme.md
@@ -0,0 +1,12 @@
+# Nihongo Word DB
+
+This database contains **216,381 words**.
+
+## Word of the day
+
+## Sources
+
+- **Tags:** [JapanDict](https://www.japandict.com)  
+- **Words:** `jmdict-eng-3.6.2`  
+- **JLPT levels:** [open-anki-jlpt-decks](https://github.com/jamsinclair/open-anki-jlpt-decks)  
+- **JLPT tags:** JapanDict
---
+++ script/select_word_of_the_day.py
@@ -0,0 +1,37 @@
+import json
+from datetime import datetime
+
+def main():
+    with open("../Words.json", "r", encoding="utf-8") as f:
+        data = json.load(f)
+
+    index = datetime.now().toordinal() % len(data)
+    entry = data[index]
+
+    # New section to insert
+    new_section = f"""## Word of the day
+
+### 🇯🇵 {entry['Japanese']}
+- **Reading:** {entry['ReadableVersion']}
+- **Translation:** {entry['EnglishTranslation']}
+- **Type:** {entry['Tags']}
+
+"""
+
+    with open("../README.md", "r", encoding="utf-8") as f:
+        content = f.read()
+
+    start = content.find("## Word of the day")
+    end = content.find("## Sources")
+
+    # If the sections are not found
+    if start == -1 or end == -1:
+        raise ValueError("Sections 'Word of the day' ou 'Sources' introuvables dans README.md")
+
+    updated_content = content[:start] + new_section + content[end:]
+
+    with open("../README.md", "w", encoding="utf-8") as f:
+        f.write(updated_content)
+
+if __name__ == "__main__":
+    main()
